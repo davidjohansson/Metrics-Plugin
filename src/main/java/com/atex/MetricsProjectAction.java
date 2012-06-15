63b660254530be4f6241da8ada938e65cb2f70a5
davidjohansson <david.johansson@atex.com>
2012-06-15 10:08:06 +0200

    Reinserted shouldreloadgraph

--- src/main/java/com/atex/MetricsProjectAction.java
+++ src/main/java/com/atex/MetricsProjectAction.java
@@ -38,10 +38,10 @@ public class MetricsProjectAction extends AbstractMetricsAction {
 
 	private DataSetBuilder<String, NumberOnlyBuildLabel> combinedGraphBuilder;
 	private TreeMap<String, DataSetBuilder<String, NumberOnlyBuildLabel>> individualGraphBuilders;
-	
-	
-	public List<String> getIndividualGraphBuilders(){
-		List<String> list = new ArrayList<String>(individualGraphBuilders.keySet());
+
+	public List<String> getIndividualGraphBuilders() {
+		List<String> list = new ArrayList<String>(
+				individualGraphBuilders.keySet());
 		Collections.sort(list);
 		return list;
 	}
@@ -54,8 +54,7 @@ public class MetricsProjectAction extends AbstractMetricsAction {
 		return project;
 	}
 
-	
-	public void init()  {
+	public void init() {
 		combinedGraphBuilder = new DataSetBuilder<String, NumberOnlyBuildLabel>();
 
 		individualGraphBuilders = new TreeMap<String, DataSetBuilder<String, NumberOnlyBuildLabel>>();
@@ -89,37 +88,34 @@ public class MetricsProjectAction extends AbstractMetricsAction {
 		}
 	}
 
-	
 	public void doMetricsIndividualGraph(StaplerRequest request,
 			StaplerResponse response) throws IOException {
 		String key = request.getParameter("key");
-		DataSetBuilder<String, NumberOnlyBuildLabel> builder = individualGraphBuilders.get(key);
-		if(builder != null){
+		DataSetBuilder<String, NumberOnlyBuildLabel> builder = individualGraphBuilders
+				.get(key);
+		if (builder != null) {
 			ChartUtil.generateGraph(request, response,
 					createMetricsGraph(builder), 800, 400);
 		}
 	}
-	
-	
+
 	public void doMetricsAccumulatedGraph(StaplerRequest request,
 			StaplerResponse response) throws IOException {
-		
-		if (shouldReloadGraph(request, response)) {
 
-		
-			//Draw the common graph:
+		if (shouldReloadGraph(request, response)) {
+			// Draw the common graph:
 			ChartUtil.generateGraph(request, response,
 					createMetricsGraph(combinedGraphBuilder), 1200, 600);
-	
+
 		}
 	}
 
-	private void addDataToBuilder(DataSetBuilder<String, NumberOnlyBuildLabel> builder, MetricsData data,
-			AbstractBuild build) {
+	private void addDataToBuilder(
+			DataSetBuilder<String, NumberOnlyBuildLabel> builder,
+			MetricsData data, AbstractBuild build) {
 		builder.add(new Integer(data.getTotalTime()), data.getKey(),
 				new NumberOnlyBuildLabel(build));
 	}
-
 
 	private JFreeChart createMetricsGraph(
 			DataSetBuilder<String, NumberOnlyBuildLabel> graphBuilder) {
@@ -154,9 +150,8 @@ public class MetricsProjectAction extends AbstractMetricsAction {
 
 	private boolean shouldReloadGraph(StaplerRequest request,
 			StaplerResponse response) throws IOException {
-		return true;
-		//		return shouldReloadGraph(request, response,
-				//project.getLastSuccessfulBuild());
+		return shouldReloadGraph(request, response,
+				project.getLastSuccessfulBuild());
 	}
 
 }
